4da8369479c412f26692438953ad4762398569cd
Samuel Machado Duarte <samuelmachado41@gmail.com>
2024-09-17 18:21:43 -0300

    JavaScript - Adicionar itens e lista de comprados

---
+++ js/criarItem.js
@@ -0,0 +1,83 @@
+import { verificarListaComprados } from "./verificarListaComprados.js";
+
+const listaComprados = document.getElementById("lista-comprados");
+const listaDeCompras = document.getElementById("lista-de-compras");
+
+let contador = 0;
+
+export function criarItemDaLista(item){
+    const itemDaLista = document.createElement("li");
+    const containerItemLista = document.createElement("div");
+    containerItemLista.classList.add("item-lista-container");
+    const containerNomeDoItem = document.createElement("div");
+    containerNomeDoItem.classList.add("container-nome-compra");
+    const containerCheckBox = document.createElement("div");
+    containerCheckBox.classList.add("checkbox-container");
+    const checkboxInput = document.createElement("input");
+    checkboxInput.type = "checkbox";
+    checkboxInput.classList.add("checkbox-input");
+    checkboxInput.id = "checkbox-" + contador++;
+    const checkboxLabel = document.createElement("label");
+    checkboxLabel.setAttribute("for", checkboxInput.id);
+    checkboxLabel.addEventListener("click", function(evento) {
+        const checkboxInput = evento.currentTarget.querySelector(".checkbox-input");
+        const checkboxCustom = evento.currentTarget.querySelector(".checkbox-customizado");
+        const itemTitulo = evento.currentTarget.closest("li").querySelector("#item-titulo");
+
+        if(checkboxInput.checked){
+            checkboxCustom.classList.add("checked");
+            itemTitulo.style.textDecoration = "line-through";
+            listaComprados.appendChild(itemDaLista);
+        } else {
+            checkboxCustom.classList.remove("checked");
+            itemTitulo.style.textDecoration = "none";
+            listaDeCompras.appendChild(itemDaLista);
+        }
+
+        verificarListaComprados(listaComprados)
+    })
+
+    const checkboxCustom = document.createElement("div");
+    checkboxCustom.classList.add("checkbox-customizado");
+
+    checkboxLabel.appendChild(checkboxInput);
+    checkboxLabel.appendChild(checkboxCustom);
+    containerCheckBox.appendChild(checkboxLabel);
+    containerNomeDoItem.appendChild(containerCheckBox);
+
+    const nomeDoItem = document.createElement("p");
+    nomeDoItem.id = "item-titulo";
+    nomeDoItem.innerText = item;
+    containerNomeDoItem.appendChild(nomeDoItem);
+
+    const containerBotoes = document.createElement("div");
+    const botaoRemover = document.createElement("button");
+    botaoRemover.classList.add("botao-acao");
+    const imagemRemover = document.createElement("img");
+    imagemRemover.src = "img/delete.svg";
+    imagemRemover.alt = "Remover";
+    botaoRemover.appendChild(imagemRemover);
+    
+
+    const botaoEditar = document.createElement("button");
+    botaoEditar.classList.add("botao-acao");
+    const imagemEditar = document.createElement("img");
+    imagemEditar.src = "img/edit.svg";
+    imagemEditar.alt = "Editar";
+    botaoEditar.appendChild(imagemEditar);
+    containerBotoes.appendChild(botaoRemover);
+    containerBotoes.appendChild(botaoEditar);
+
+    containerItemLista.appendChild(containerNomeDoItem);
+    containerItemLista.appendChild(containerBotoes);
+    
+    const itemData = document.createElement("p");
+    itemData.innerText = `${new Date().toLocaleDateString("pt-BR", { weekday: "long"})} (${new Date().toLocaleDateString("pt-BR")}) às ${new Date().toLocaleTimeString("pt-BR", {hour: "numeric", minute: "numeric"})}`;
+    itemData.classList.add("texto-data");
+
+
+    itemDaLista.appendChild(containerItemLista);
+    itemDaLista.appendChild(itemData);
+
+    return itemDaLista;
+}
--- scripts.js
+++ scripts.js
@@ -1,16 +1,8 @@
-const item = document.getElementById("input-item");
-const botaoSalvarItem = document.getElementById("adicionar-botao");
+import { adicionarItem } from "./js/AdicionarItem.js";
+import { verificarListaComprados } from "./js/verificarListaComprados.js";
 
+const botaoSalvarItem = document.getElementById("adicionar-botao");
 botaoSalvarItem.addEventListener("click", adicionarItem);
 
-function adicionarItem(evento) {
-    evento.preventDefault();
-    console.log("Entrei na função");
-
-    const itemDaLista = document.createElement("li");
-    const containerItemLista = document.createElement("div");
-    containerItemLista.classList.add("lista-item-container");
-
-    const con
-    itemDaLista.appendChild(containerItemLista);
-}
+const listaComprados = document.getElementById("lista-comprados");
+verificarListaComprados(listaComprados);
